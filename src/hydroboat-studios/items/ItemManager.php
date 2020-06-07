9bc9a8b66fb524499dd1d395c6589f2ae728f298
HydroBoatStudios <66497242+HydroBoatStudios@users.noreply.github.com>
2020-06-07 17:11:09 -0400

    Create ItemManager.php

---
+++ src/hydroboat-studios/items/ItemManager.php
@@ -0,0 +1,27 @@
+<?php
+
+
+ * Berry (c) 2020
+ * This project is licensed under GNU GENERAL PUBLIC LICENSE v3.0
+ * It is free to use, and copyright free.
+ *
+ * @author hydroboat-studios
+ *
+
+declare(strict_types=1);
+
+namespace hydroboat-studios/src/hydroboat-studios/items;
+
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+
+class ItemManager {
+
+    public function __construct() {
+        $this->init();
+    }
+
+    public function init() {
+        ItemFactory::registerItem(new SweetBerries(), true);
+        Item::initCreativeItems();
+    }
