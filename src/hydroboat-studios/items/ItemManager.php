d9855d235c067d4d8cb8afe4adc48567c0bd48a1
HydroBoatStudios <66497242+HydroBoatStudios@users.noreply.github.com>
2020-06-07 17:19:32 -0400

    Update ItemManager.php

--- src/hydroboat-studios/items/ItemManager.php
+++ src/hydroboat-studios/items/ItemManager.php
@@ -10,7 +10,7 @@
 
 declare(strict_types=1);
 
-namespace hydroboat-studios/src/hydroboat-studios/items;
+namespace src/hydroboat-studios/items;
 
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
