c4a6547e54633449b3a94f1f366f042203c42192
HydroBoatStudios <66497242+HydroBoatStudios@users.noreply.github.com>
2020-06-10 13:36:34 -0400

    Update ItemManager.php

--- src/hydroboat-studios/items/ItemManager.php
+++ src/hydroboat-studios/items/ItemManager.php
@@ -1,1 +1,17 @@
+<?php
 
+declare(strict_types = 1);
+
+namespace hydroboat-stuios\items;
+
+use hyroboat-studios\Main;
+use pocketmine\item\{
+	Item, ItemFactory
+};
+
+class ItemManager {
+	public static function init(){
+		ItemFactory::registerItem(new SweetBerries(), true);
+		ItemFactory::registerItem(new Honeycomb(), true);
+		ItemFactory::registerItem(new HoneyBottle(), true);
+	
